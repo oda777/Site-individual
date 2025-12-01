603a343bd90ed54fef818dba32da4bbd546ae209
oda777 <murilo.augusto.barbosa@escola.pr.gov.br>
2025-12-01 09:10:01 -0300

    .

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-
+document.querySelectorAll('section').forEach(function(section) {
+    section.addEventListener('click', function() {
+      
+        const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+        section.style.backgroundColor = randomColor;
+    });
+});
